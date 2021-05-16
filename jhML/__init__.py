54c22cb72b23f4dd6c6e649473d7bcf0db8885bf
Janghyun Son <jhson989@gmail.com>
2021-05-16 17:48:33 +0900

    jhML project started

---
+++ jhML/__init__.py
@@ -0,0 +1,19 @@
+
+r"""
+jhML : A Useful Deep Learning Framework Implemented by jhson, Korea
+jhML framework is originated from "dezero" project, and some 
+more useful utilities are added. Feel free to use this framework to study
+concepts of modern deep learning frameworks and a dynamic graph.
+jhML contains data structures for multi-dimensional tensors 
+and defines mathematical operations over these tensors.
+Additionally, it provides some utilities for developing deep learning applications
+
+It will have a GPGPU counterpart, so you can run your tensor computations on an GPU someday
+"""
+
+from jhML.core import Variable
+from jhML.core import Function
+
+setup_variable()
+__version__ = "0.0.1"
+
