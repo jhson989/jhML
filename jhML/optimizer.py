9e6d0772fd185341d9e4ff672c7c388c5464cfc5
Janghyun Son <jhson989@gmail.com>
2021-05-23 20:50:53 +0900

    Added weight_decay (l2 penalty)

--- jhML/optimizer.py
+++ jhML/optimizer.py
@@ -1,27 +1,46 @@
+from jhML.core import Variable
 import math
 import numpy as np
 
 
+
+
+
+##################################################################3
+# Optimizer base class #
+# : Update weights of a variable
+# : Implement "update_one" procedure for each optimization algorithm
+##################################################################3
+
 class Optimizer:
 
     def __init__(self, params, lr):
+        '''
+        args:
+            params: List of variables to be updated
+            lr: learning rate
+        '''
         self.lr = lr
         self.params = params
         self.hooks = []
 
     def zero_grad(self):
-        for param in self.params():
+        for param in self.params:
             param.clear_grad()
 
     def step(self):
 
         for f in self.hooks:
-            f(params)
+            f(self.params)
 
-        for param in self.params():
+        for param in self.params:
             self.update_one(param)
 
-    def update_one(self, param):
+    def update_one(self, param: Variable):
+        '''
+        arg1:
+            param: A variable to be updated
+        '''
         raise NotImplementedError()
 
     def add_hook(self, f):
@@ -30,26 +49,51 @@ class Optimizer:
 
 
 ##################################################################3
-# Optimizer algorithm # SGD / MomentumSGD / AdaGrad
+# Optimization algorithms
+# : SGD / MomentumSGD / AdaGrad
 ##################################################################3
 
 class SGD(Optimizer):
 
-    def __init__(self, params, lr):
+    def __init__(self, params, lr, weight_decay=0.0):
+        '''
+        Stochastic Gradient Descent
+        '''
         super().__init__(params, lr)
+        self.weight_decay = weight_decay
 
-    def update_one(self, param):
+    def update_one(self, param: Variable):
+        '''
+        arg1:
+            param: A variable to be updated
+        '''
+
+        if self.weight_decay != 0.0:
+            param.grad += self.weight_decay * param.data
+
         param.data -= self.lr * param.grad
 
 
 class MomentumSGD(Optimizer):
 
-    def __init__(self, params, lr, momentum=0.9):
+    def __init__(self, params, lr, momentum=0.9, weight_decay=0.0):
+        '''
+        Stochastic Gradient Descent with Momentum
+        '''
         super().__init__(params, lr)
         self.m = momentum
         self.v = {}
+        self.weight_decay = weight_decay
 
-    def update_one(self, param):
+    def update_one(self, param: Variable):
+        '''
+        arg1:
+            param: A variable to be updated
+        '''
+
+        if self.weight_decay != 0.0:
+            param.grad += self.weight_decay * param.data
+            
         key = id(param)
         if key not in self.v:
             self.v[key] = np.zeros_like(param.data)
@@ -59,15 +103,26 @@ class MomentumSGD(Optimizer):
 
 class AdaGrad(Optimizer):
 
-    def __init__(self, params, lr, eps=1e-8):
+    def __init__(self, params, lr, weight_decay=0.0, eps=1e-8):
+        '''
+        Adaptively adjust learning rates for each parameter
+        '''
         super().__init__(params, lr)
         self.h = {}
         self.eps = eps
+        self.weight_decay = weight_decay
 
-    def update_one(self, param):
+    def update_one(self, param: Variable):
+        '''
+        arg1:
+            param: A variable to be updated
+        '''
         key = id(param)
         if key not in self.h:
             self.h[key] = np.zeros_like(param.data)
+
+        if self.weight_decay != 0.0:
+            param.grad += self.weight_decay * param.data
 
         self.h[key] = self.h[key] + param.grad * param.grad
         param.data -= self.lr * param.grad / ( self.eps + np.sqrt(self.h[key]) )
@@ -75,4 +130,8 @@ class AdaGrad(Optimizer):
 
 
 
-        
+
+##################################################################3
+# Hook functions
+# ClipGrad, FreezeParam
+##################################################################3
--- script/nn_test/test_xor_optim.py
+++ script/nn_test/test_xor_optim.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     #### learning
 #    optim = optim.SGD(params(net), lr=1e-3)
 #    optim = optim.MomentumSGD(params(net), lr=1e-4)
-    optim = optim.AdaGrad(params(net), lr=1e-3)
+    optim = optim.AdaGrad(params(net), lr=1e-3, weight_decay=1e-3)
 
 
     num_epoch = int(1e+5)
