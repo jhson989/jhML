fe8b047cd276b214f3f02f93c729c594d46ac7a5
Janghyun Son <jhson989@gmail.com>
2021-05-23 22:23:04 +0900

    [Rename] Sequential layer -> model

--- jhML/models.py
+++ jhML/models.py
@@ -1,6 +1,24 @@
 
 import numpy as np
-import jhML.layers as L
+from jhML.layers import Layer
 
-Model = L.Layer
+Model = Layer
+
+
+class Sequential(Model):
+
+    def __init__(self, *layers):
+        super().__init__()
+        self.layers = []
+        
+        for i, layer in enumerate(layers):
+            setattr(self, 'l'+str(i), layer)
+            self.layers.append(layer)
+
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
 
