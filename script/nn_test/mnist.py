429c45f1a28f2a9eda8abf317d8edbe51320de64
jhson989 <jhson989@gmail.com>
2021-06-17 14:49:14 +0900

    add code for calculating accuracy

--- script/nn_test/mnist.py
+++ script/nn_test/mnist.py
@@ -8,6 +8,13 @@ import jhML.functions as F
 import jhML.optimizer as optim
 from jhML.tutorial.dataset import MNIST
 
+def get_accuracy(pred, gt):
+    pred = jhML.as_cpu(pred)
+    gt = jhML.as_cpu(gt)
+    return np.sum((pred == gt).astype(float))/len(gt)
+    
+
+
 net = nn.Sequential(
         nn.Linear(28*28, 28*28),
         nn.ReLU(),
@@ -28,8 +35,8 @@ if __name__ == "__main__":
     num_class = 10
     dataset_mnist_train = MNIST(train=True, flatten=True)
     dataset_mnist_test = MNIST(train=False, flatten=True)
-    train_dataloader = jhML.Dataloader(dataset_mnist_train, batch_size=64, gpu=using_gpu, drop_last=True)
-    test_dataloader = jhML.Dataloader(dataset_mnist_test, batch_size=1, shuffle=False, gpu=using_gpu, drop_last=False)
+    train_dataloader = jhML.Dataloader(dataset_mnist_train, batch_size=256, gpu=using_gpu, drop_last=True)
+    test_dataloader = jhML.Dataloader(dataset_mnist_test, batch_size=256, shuffle=False, gpu=using_gpu, drop_last=False)
 
     net = net.to(using_gpu)
 
@@ -37,8 +44,10 @@ if __name__ == "__main__":
 
     #### learning
     optim = optim.RMSprop(net.params(), lr=5e-5)
-    num_epoch = int(1e+1)
+    num_epoch = int(1e+2)
     for epoch in range(num_epoch):
+        total_loss = 0.0
+        total_accuracy = 0.0
         for i, (data, label) in enumerate(train_dataloader):
             optim.zero_grad()
             pred = net(data)
@@ -46,16 +55,22 @@ if __name__ == "__main__":
             loss.backward()
             optim.step()
 
-            if epoch%1 == 0 and i%1000 == 0:
-                pred, label = jhML.as_cpu(pred), jhML.as_cpu(label)
+            total_loss += loss.data
+            total_accuracy += get_accuracy(F.argmax(pred), label.ravel())
+
+            if epoch%1 == 0 and i == (len(train_dataloader)-1):
                 print("EPOCH (%d/%d), ITER (%d/%d)" % (epoch, num_epoch, i, len(train_dataloader)))
-                print("PR: " + str(F.argmax(pred)))
-                print("GT: " + str(label.ravel()))
+                print(" acc: %f, loss: %f" % (total_accuracy/len(train_dataloader), total_loss/len(train_dataloader)))
+                #pred, label = jhML.as_cpu(pred), jhML.as_cpu(label)
+                #print("PR: " + str(F.argmax(pred)))
+                #print("GT: " + str(label.ravel()))
             
 
     print("[[TEST]] Test start")
+    total_accuracy = 0.0
     for i, (data, label) in enumerate(test_dataloader):
         pred = net(data)
-        print("PR: " + str(F.argmax(pred)))
-        print("GT: " + str(label.ravel()))
+        total_accuracy += get_accuracy(F.argmax(pred), label.ravel())
+    print(" test acc: %f" % (total_accuracy/len(test_dataloader)))
+
     
